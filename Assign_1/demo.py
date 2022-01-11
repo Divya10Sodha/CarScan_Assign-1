50ce6ddf17e9652adcc4daf5cdd8c86944c693a0
Divya Sodha <49143170+Divya10Sodha@users.noreply.github.com>
2022-01-11 14:04:14 +0530

    Update demo.py

--- Assign_1/demo.py
+++ Assign_1/demo.py
@@ -4,5 +4,5 @@ import os
 list = os.listdir('data/') 
 number_files = len(list)
 
-for i in range(number_files):
+for i in range(number_files - 1):
     image_viz(str(i+1) +'.json', str(i+1) +'.jpg')
